67450be7782aa1ed1a7647b706be76d1eefaa94f
João Soares <jpcirqueira81@gmail.com>
2020-11-13 19:43:35 -0300

    Adicionado bubblesort a função de gerargrafico.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- geragrafico.py
+++ geragrafico.py
@@ -3,14 +3,16 @@ import timeit
 import matplotlib.pyplot as plt
 import mergesort
 import vetoraleatorio
+import bubblesort
 
 vet1 =[]
 vet2 =[]
+vet3 =[]
 
 def main():
     tamanho=1
 
-    for x in range(10):
+    for x in range(6):
 
         inicio=0
         fim=10000
@@ -23,8 +25,21 @@ def main():
         tempo = (time.time() - ini)
         vet2.append(round(tempo, 6))
 
-        # print("tempo de execução: %.6f segundos " % tempo)
-        tamanho = tamanho * 4
+        ini2 = time.time()
+        bubblesort.main(vetor)
+        tempo = (time.time() - ini2)
+        vet3.append(round(tempo, 6))
 
-    plt.plot(vet1,vet2)
+        # print("tempo de execução: %.6f segundos " % tempo)
+        tamanho = tamanho * 5
+
+    #  [1,2,3,4,5]
+    #  [0,50000,100000,150000,200000,250000]
+    plt.plot(vet1,vet2,label='mergesort')
+    plt.plot(vet1,vet3,label='bubblesort')
+    plt.grid(True)
+    plt.ylabel('tempo')
+    plt.xlabel('vetor')
+    plt.title('Grafico tempo x tamanho')
+    plt.legend()
     plt.show()
