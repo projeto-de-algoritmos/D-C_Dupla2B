202a0ffa43d7a4604dc30cd73ed9ee6c921e17f6
João Soares <jpcirqueira81@gmail.com>
2020-11-13 21:18:57 -0300

    Adicionado menu.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import geragrafico
 
-geragrafico.main()
+def print_menu():
+    print(30 * "-" , "MENU" , 30 * "-")
+    print("1. Mostra grafico mergesort")
+    print("2. Mostra grafico comparando mergesort e bublesort") 
+    print("3. Sair")
+    print(67 * "-")
+
+
+if __name__ == '__main__':
+    loop=True
+    
+    while loop:
+        print_menu()
+        choice = input("Entre sua opcao [1-4]: ")            
+        if choice=='1':
+            print("Opcao 1 foi escolhida")
+            geragrafico.main()
+        elif choice=='2':
+            print("Opcao 2 foi escolhida")
+            geragrafico.main()
+        elif choice=='3':
+            print("Opcao 4 foi escolhida")
+            print('Saindo....')
+            loop=False
+        else:
+            input("Opcao incorreta. Aperte qualquer tecla para tentar novamente..")
